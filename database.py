348b91a86ee094286fdc6a78520980c56c3c0f6d
Kerbx <r3ostat@ya.ru>
2024-07-21 19:56:31 +0300

    mvp done.

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
+import karma
+
+import logging
 import peewee
+import telebot
 
 
 db = peewee.SqliteDatabase('users.db')
@@ -9,14 +13,45 @@ class User(peewee.Model):
     name = peewee.CharField()
     username = peewee.CharField()
     karma = peewee.IntegerField()
-    admin = peewee.BooleanField()
     warns = peewee.IntegerField()
     state = peewee.IntegerField()
     
     class Meta:
         database = db
         
-        
+
+def get_warns_count(user: telebot.types.User):
+    karma.check_user_in_database(user)
+    _user = User.select().where(User.id == str(user.id)).get()
+    return _user.warns
+
+
+def give_warn(user: telebot.types.User):
+    karma.check_user_in_database(user)
+    _user = User.select().where(User.id == str(user.id)).get()
+    _user.warns += 1
+    try:
+        _user.save()
+        logging.info(f'User {user.full_name} ({user.username}) got warn (now {get_warns_count(user)})!')
+    except Exception as exception:
+        logging.error(exception)
+        return 'err'
+    else:
+        if get_warns_count(_user) > 3:
+            return 'muted'
+    
+    
+def clear_warns(user: telebot.types.User):
+    karma.check_user_in_database(user)
+    _user = User.select().where(User.id == str(user.id)).get()
+    _user.warns = 0
+    try:
+        _user.save()
+        logging.info(f'User {user.full_name} ({user.username}) forgiven.')
+    except Exception as exception:
+        logging.error(exception)
+    
+    
 if __name__ == '__main__':
     db.connect()
     db.create_tables([User, ])
--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import config
 import database
 import globals
+import karma
 
 import asyncio
 import logging
-import karma
 import telebot.async_telebot
+import string
 
 
 logging.basicConfig(level=logging.INFO, filename="epta_logi.log",filemode="a",
-                    format="%(asctime)s %(levelname)s %(message)s")
+                    format="%(asctime)s %(levelname)s %(message)s", encoding='utf-8')
 
 bot = telebot.async_telebot.AsyncTeleBot(config.BOT_TOKEN)
 
 
+async def get_message_reply_user(message):
+    user = None
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user
+        if message.reply_to_message.from_user.is_bot:
+            return None
+        return user
+    
+    
 @bot.message_handler(chat_types=['private'], commands=['/start'])
 async def start(message):
     await bot.send_message(message.chat.id, 'Привет, я Годетта, работаю пока что только в группе, личку не люблю...')
@@ -36,9 +46,9 @@ async def welcome_message(message):
                                 message_thread_id=globals.THREADS.get('GREETING_THREAD'),
                                 text=greeting_text,
                                 parse_mode='MarkdownV2',
-                                link_preview_options=telebot.types.LinkPreviewOptions(True))
+                                link_preview_options=telebot.types.LinkPreviewOptions(False))
     except Exception as exception:
-        logging.error(exception)
+        logging.exception(exception)
         await bot.send_message(message.chat.id,
                                message_thread_id=globals.THREADS.get('GREETING_THREAD'),
                                text="Упс, что-то пошло не так!")
@@ -49,25 +59,82 @@ async def clear_leave_message(message):
     await bot.delete_message(message.chat.id, message.id)
     
 
+@bot.message_handler(chat_types=['supergroup'], commands=['warn'])
+async def warn_user(message):
+    admins = await bot.get_chat_administrators(message.chat.id)
+    admins_id = [id.user.id for id in admins]
+    if message.from_user.id in admins_id:
+        if message.reply_to_message.from_user.id in admins_id:
+            await bot.reply_to(message, 'Господа админы, прошу выяснять отношения в личных сообщениях.')
+            return
+        result = database.give_warn(message.reply_to_message.from_user)
+        if result == 'err':
+            await bot.reply_to(message, 'Произошла ошибка, чекни логи.')
+        elif result == 'muted':
+            await bot.reply_to(message, f'Пользователь {message.reply_to_message.from_user.full_name} ({karma.get_user_karma(message.reply_to_message.from_user)}) получил последнее предупреждение.\nТеперь посидит в молчанке.')
+            await bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id,
+                                           None, False, False, False, False, False, False,
+                                           False, False)
+        else:
+            await bot.reply_to(message, f'Пользователь {message.reply_to_message.from_user.full_name} ({karma.get_user_karma(message.reply_to_message.from_user)}) получил предупреждение.\nСейчас их {database.get_warns_count(message.reply_to_message.from_user)}.')
+        
+
+@bot.message_handler(chat_types=['supergroup'], commands=['unmute'])
+async def unmute_user(message):
+    admins = await bot.get_chat_administrators(message.chat.id)
+    admins_id = [id.user.id for id in admins]
+    if message.from_user.id in admins_id:
+        if message.reply_to_message.from_user.id in admins_id:
+            await bot.reply_to(message, 'Господа админы, прошу выяснять отношения в личных сообщениях.')
+            return
+        database.clear_warns(message.reply_to_message.from_user)
+        await bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id,
+                                       None, True, True,)
+        await bot.reply_to(message, f'Так и быть, выходи из молчанки, [{message.reply_to_message.from_user.first_name}](tg://user?id={message.reply_to_message.from_user.id})\.',
+                           parse_mode='MarkdownV2')
+        
+        
+@bot.message_handler(chat_types=['supergroup'], commands=['ban'])
+async def ban_user(message):
+    admins = await bot.get_chat_administrators(message.chat.id)
+    admins_id = [id.user.id for id in admins]
+    if message.from_user.id in admins_id:
+        if message.reply_to_message.from_user.id in admins_id:
+            await bot.reply_to(message, 'Господа админы, прошу выяснять отношения в личных сообщениях.')
+            return
+        await bot.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+        await bot.reply_to(message, f'Пользователь {message.reply_to_message.from_user.full_name} теперь забанен. Мы не жалеем.')
+        logging.info(f'User {message.reply_to_message.from_user.full_name} ({message.reply_to_message.from_user.username}) got banned.')        
+
+
+@bot.message_handler(chat_types=['supergroup'], commands=['stats'])
+async def get_user_stats(message):
+    user = await get_message_reply_user(message)
+    user = message.from_user if not user else user
+    await bot.reply_to(message, f'Карма пользователя {user.full_name} сейчас составляет {karma.get_user_karma(user)}.')
+
+
 @bot.message_handler(chat_types=['supergroup'], func=lambda message: True)
 async def listen_to_karma(message):
     # govnokod starts here.
     if message.text.startswith(globals.KARMA_THANKS):
+        if message.from_user.id == message.reply_to_message.from_user.id:
+            await bot.reply_to(message, 'Я понимаю, что ты самовлюбленный дурак, но не нужно этого.')
+            return
         karma.check_user_in_database(message.from_user)
         karma.check_user_in_database(message.reply_to_message.from_user)
         karma.change_user_karma(message.reply_to_message.from_user)
-        await bot.reply_to(message, f'{message.from_user.first_name} ({karma.get_user_karma(message.from_user)})\
-                            повысил карму {message.reply_to_message.from_user}\
-                            ({karma.get_user_karma(message.reply_to_message.from_user)}).')
+        await bot.reply_to(message, f'{message.from_user.first_name} ({karma.get_user_karma(message.from_user)}) повысил карму {message.reply_to_message.from_user.first_name} ({karma.get_user_karma(message.reply_to_message.from_user)}).')
     elif message.text.startswith(globals.KARMA_CONDEMNATION):
+        if message.from_user.id == message.reply_to_message.from_user.id:
+            await bot.reply_to(message, 'Я понимаю, что ты самокритичный дурак, но не нужно этого.')
+            return
         karma.check_user_in_database(message.from_user)
         karma.check_user_in_database(message.reply_to_message.from_user)
-        karma.change_user_karma(message.reply_to_message, -1)
-        await bot.reply_to(message, f'{message.from_user.first_name} ({karma.get_user_karma(message.from_user)})\
-                            понизил карму {message.reply_to_message.from_user}\
-                            ({karma.get_user_karma(message.reply_to_message.from_user)}).')
+        karma.change_user_karma(message.reply_to_message.from_user, -1)
+        await bot.reply_to(message, f'{message.from_user.first_name} ({karma.get_user_karma(message.from_user)}) понизил карму {message.reply_to_message.from_user.first_name} ({karma.get_user_karma(message.reply_to_message.from_user)}).')
 
 
 if __name__ == '__main__':
-    asyncio.run(bot.polling())
+    asyncio.run(bot.infinity_polling())
     
