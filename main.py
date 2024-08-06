aa2495485c425103b66461bdedee5e951116b0a1
Kerbx <r3ostat@ya.ru>
2024-08-06 22:43:28 +0300

    fixed karma to media messages.

--- main.py
+++ main.py
@@ -145,7 +145,8 @@ async def get_user_stats(message):
 @bot.message_handler(chat_types=['supergroup'], func=lambda message: True)
 async def listen_to_karma(message):
     # govnokod starts here.
-    if not message.reply_to_message.text:
+    print(message)
+    if message.reply_to_message.forum_topic_created:
         return
     if message.text.lower().startswith(globals.KARMA_THANKS):
         if message.from_user.id == message.reply_to_message.from_user.id:
